3bedfd8d7f035df82356e91329850b8a63df60a0
rodrigopk <rodrigopk@gmail.com>
2020-04-26 11:30:33 +0200

    Add user model

---
+++ db/migrations/20200426092407_create_users.rb
@@ -0,0 +1,19 @@
+Hanami::Model.migration do
+  change do
+    create_table :users do
+      primary_key :id,
+                  'uuid',
+                  null: false,
+                  default: Hanami::Model::Sql.function(:uuid_generate_v4)
+
+
+      column :email, String, null: false
+      column :password_digest, String, null: false
+      column :first_name, String, null: false
+      column :last_name, String, null: false
+
+      column :created_at, DateTime, null: false
+      column :updated_at, DateTime, null: false
+    end
+  end
+end
