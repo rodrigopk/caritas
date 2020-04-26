791574699fce559f0357c0abc41cf96cd9202ca6
rodrigopk <rodrigopk@gmail.com>
2020-04-26 12:45:09 +0200

    Add interactor to handle signup

---
+++ lib/caritas/interactors/users/signup.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'hanami/interactor'
+
+module Interactors
+  module Users
+    class Signup
+      include Hanami::Interactor
+
+      expose :user
+
+      def initialize(user_attributes:, dependencies: {})
+        @user_attributes = user_attributes
+
+        inject_dependencies(dependencies)
+      end
+
+      def call
+        @user = @user_repository.create(
+          email: @user_attributes[:email],
+          password_digest: hashed_password,
+          first_name: @user_attributes[:first_name],
+          last_name: @user_attributes[:last_name],
+        )
+      end
+
+      private
+
+      def inject_dependencies(dependencies)
+        @user_repository = dependencies.fetch(:repository) do
+          Containers::Institutions[:repository]
+        end
+
+        @password_service = dependencies.fetch(:password_service) do
+          Containers::Services[:password]
+        end
+      end
+
+      def hashed_password
+        hashed_password = @password_service.encrypt(@user_attributes[:password])
+      end
+    end
+  end
+end
---
+++ spec/caritas/interactors/users/signup_spec.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Interactors::Users::Signup, type: :interactor do
+  let(:user_attributes) do
+    {
+      email: 'penelope@cruz.com',
+      password: 'superSecretPassword',
+      first_name: 'Penelope',
+      last_name: 'Cruz',
+    }
+  end
+
+  it 'initializes without dependencies' do
+    described_class.new(user_attributes: user_attributes)
+  end
+
+  describe 'call' do
+    let(:interactor) do
+      described_class.new(
+        user_attributes: user_attributes,
+        dependencies: dependencies,
+      )
+    end
+
+    let(:user_repository) { instance_double(UserRepository) }
+    let(:password_service) { class_double(Services::Password) }
+    let(:dependencies) do
+      {
+        repository: user_repository,
+        password_service: password_service,
+      }
+    end
+    let(:user) { instance_double(User) }
+    let(:hashed_password) { 'encrypted_password' }
+
+    before do
+      allow(password_service).to encrypt_password
+      allow(user_repository).to create_user
+    end
+
+    it 'calls' do
+      interactor.call
+    end
+
+    it 'encrypts the given password' do
+      expect(password_service).to encrypt_password
+
+      interactor.call
+    end
+
+    it 'fetchs all institutions' do
+      expect(user_repository).to create_user
+
+      interactor.call
+    end
+
+    it 'exposes the retrieved institutions' do
+      context = interactor.call
+
+      expect(context.user).to equal(user)
+    end
+  end
+
+  private
+
+  def encrypt_password
+    receive(:encrypt)
+      .with(user_attributes[:password])
+      .and_return(hashed_password)
+  end
+
+  def create_user
+    receive(:create)
+      .with(
+        email: user_attributes[:email],
+        password_digest: hashed_password,
+        first_name: user_attributes[:first_name],
+        last_name: user_attributes[:last_name],
+      )
+      .and_return(user)
+  end
+end
